5eab7eb5d42e910893ad9a73e44b42b16a9d4562
Evanraboutot <95092081+Evanraboutot@users.noreply.github.com>
2023-03-20 08:18:30 +0100

    Add files via upload

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+const Header = () => {
+  return (
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          {/* logo */}
+          {/* boutton */}
+
+        </div>
+      </div>
+    </header>
+  );
+};
+
+
+export default Header;
